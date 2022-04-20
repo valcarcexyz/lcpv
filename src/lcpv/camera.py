fba27b9888f70fbee2e2fc70c06a2335e96a4efa
Diego Valcarce <d.valcarce@udc.es>
2022-04-20 11:21:56 +0200

    camera runs

--- src/lcpv/camera.py
+++ src/lcpv/camera.py
@@ -17,29 +17,34 @@ class Camera:
                         process_output: Callable = lambda x: x,
                         ):
         """
+        Abstraction layer of the picamera interface. Captures frames using the video port (same resolution, better
+        framerate). For parallel usage, it provides a `running` thread-safe variable, so different threads can check
+        whether it is running or it has already stopped.
+
         Params:
         ======
-        resolution: tuple. Resolution to capture video (defaults to 1080p).
-        framerate: int, defaults to 24.
-        seconds: int. How long to capture the images
-        process_output: function. What to do with the captured frames (frames will be a numpy array)
+        :param resolution: tuple. Resolution to capture video (defaults to 1080p).
+        :param framerate: int, defaults to 24.
+        :param seconds: int. How long to capture the images
+        :param process_output: function. What to do with the captured frames (frames will be a numpy array)
         """
-        self.running.value = 1
+        self.running.value = 1  # set 'boolean' to running
         with picamera.PiCamera(resolution=resolution, framerate=framerate) as camera:
             camera.capture_sequence(
-                    self._gen_buffers(frames=seconds * framerate,
-                                      process_output=process_output,
-                                      resolution=resolution),
-                    "yuv",
-                    use_video_port=True
+                self._gen_buffers(frames=seconds * framerate,  # number of frames to capture with the video port
+                                  process_output=process_output,
+                                  resolution=resolution),
+                "yuv",  # fastest method tested
+                use_video_port=True
             )
             print("Capture process ended. Closing the camera.")
         print("Camera closed.")
-        self.running.value = 0
+        self.running.value = 0  # no longer running
         return True
 
-    def _gen_buffers(self, frames: int, process_output: Callable, resolution: tuple):
-        """Yields buffers"""
+    @staticmethod
+    def _gen_buffers(frames: int, process_output: Callable, resolution: tuple):
+        """Yields buffers and runs the `process_output` function to the captured frames"""
         for _ in range(frames):
             # create the stream buffer (to capture the frame to)
             stream = io.BytesIO()
@@ -52,4 +57,4 @@ class Camera:
                 count=np.prod(resolution),
             ).reshape(resolution[::-1])
             process_output(image)
-        return
+        return None

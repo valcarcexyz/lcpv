cf997b4d03f8751eff72bf0d39e26d52ca22b824
Diego Valcarce <d.valcarce@udc.es>
2022-04-21 08:32:28 +0200

    Added all the authors in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(
     packages=find_packages(where="src"),
     url='https://github.com/valcarce01/lcpv',
     license='',
-    author='Diego Valcarce et al',
+    author='Diego Valcarce Ríos,\
+            Juan Ramón Rabuñal Dopico,\
+            Juan Naves García-Rendueles,\
+            José Anta Álvarez,\
+            Sonia Seijo Conchado',
     author_email='d.valcarce@udc.es',
     description='Low cost (Rasperry-based) particle velociemtry',
     install_requires=[line.strip() for line in open("requirements.txt").readlines()],

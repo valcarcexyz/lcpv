6266f56c6d7fa4127f3de74d64159bc6278efa7a
Diego Valcarce <diego@diegovalcarce.es>
2022-09-20 10:39:58 +0200

    example from video

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from lcpv import LCPV
 from lcpv.preprocessing.filters import opening_filter
+import numpy as np
 
 ## lcpv
 l = LCPV(
@@ -7,5 +8,9 @@ l = LCPV(
     preprocessing_filter=opening_filter,
 )
 
+l.process_video("data/video.mp4")
 
+u, v = l.results["u"], l.results["v"]
+print(np.nanmean(u, axis=0))
+print(np.nanmean(v, axis=0))
 

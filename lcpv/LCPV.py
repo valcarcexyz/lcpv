7f757c8af25a2a092fdd23f1562e948cc569bc3f
Diego Valcarce <diego@diegovalcarce.es>
2022-09-20 11:00:09 +0200

    camera processing

--- lcpv/LCPV.py
+++ lcpv/LCPV.py
@@ -6,8 +6,10 @@ from openpiv.pyprocess import extended_search_area_piv, get_coordinates
 import numpy as np
 import cv2
 import os
-
 from tqdm import tqdm
+
+from .camera import Camera
+from lcpv import camera
 
 
 class LCPV():
@@ -24,7 +26,22 @@ class LCPV():
             postprocessing_filter_args: dict = {},
             *args, **kwargs
         ) -> None:
-        """"""
+        """
+        Params: 
+        =======
+        :param preprocessing_filter: Callable
+            A function to be run to preprocess the frames
+        :param preprocessing_filter_args: dict
+            The arguments to `preprocessing_filter`, as a dictionary
+        :param piv_*: int
+            Refer to OpenPIV documentation [1]
+        :param postprocessing_filter: Callable
+            A function to be run after computing the velocities
+        :param postprocessing_filter_args: dict 
+            The arguments to `postprocessing_filter`, as a dictionary
+
+        [1]: http://www.openpiv.net/openpiv-python/
+        """
         self.preprocessing = lambda x: preprocessing_filter(x, **preprocessing_filter_args)
         self.piv_process = lambda x, y: self._piv(x, y,
             window_size = piv_window_size,
@@ -35,19 +52,17 @@ class LCPV():
         self.postprocessing = lambda x: postprocessing_filter(x, postprocessing_filter_args)
         self.results = {"x": [], "y": [], "u": [], "v": []}
 
-    def _piv(self, frame0: np.ndarray, frame1, *args, **kwargs):
-        u, v, s2n = extended_search_area_piv(frame0, frame1, *args, **kwargs)
+    def _piv(self, frame0: np.ndarray, frame1: np.ndarray, *args, **kwargs):
+        u, v, _ = extended_search_area_piv(frame0, frame1, *args, **kwargs)
         x, y = get_coordinates(
             image_size=frame0.shape,
             search_area_size=kwargs["search_area_size"],
             overlap=kwargs["overlap"],
         )
-        # valid = s2n > np.percentile(s2n, 5)
         return x, y, u, -v
 
 
-    def _consume_frames(self, frame1, frame2) -> None:
-        """"""
+    def _consume_frames(self, frame1: np.ndarray, frame2: np.ndarray) -> None:
         frame1, frame2 = self.preprocessing(frame1), self.preprocessing(frame2)
         x, y, u, v = self.piv_process(frame1, frame2)
         self.results["x"].append(x)
@@ -58,11 +73,34 @@ class LCPV():
     def process_camera(self,
             resolution: tuple = (1920, 1080),
             fps: int = 24,
+            seconds: int = 1,
+            process_output: Callable = lambda x: x,
         ) -> None:
-        """"""
+        """
+        Params:
+        =======
+        :param resolution: tuple[int]
+        :param fps: int
+        :param seconds: int 
+            How long to capture frames
+        :process_output: Callable
+            A function to be run after capturing each frame
+        """
+        camera = Camera()
+        camera.start_recording(
+            resolution=resolution,
+            framerate=fps,
+            seconds=seconds,
+            process_output=process_output,
+        )
     
     def process_video(self, filename: str) -> None:
-        """"""
+        """
+        Params:
+        =======
+        :param filename: str
+            Path to the video to process
+        """
         if not os.path.exists(filename):
             raise FileNotFoundError(f"File {filename} not found")
 
